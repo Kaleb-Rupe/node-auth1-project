c41d4763b8c21d3afc2a2ea64b86054eaf6ee4a0
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2023-09-19 21:58:51 -0400

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -51,7 +51,13 @@ router.post(
  */
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
-  res.json("login");
+  const { password } = req.body;
+  if (bcrypt.compareSync(password, req.user.password)) {
+    req.session.user = req.user;
+    res.json({ message: `Welcome ${req.user.username}` });
+  } else {
+    next({ status: 401, message: `Invalid credentials` });
+  }
 });
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
